e81ed7ce12d4574c7ba6269e26fb1f1846e76587
aiden <some09one@gmail.com>
2019-12-21 14:04:16 +0800

    modify backdesk sidebar navbar RWD

--- src/components/backdesk/sideBar.vue
+++ src/components/backdesk/sideBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-    <div class="sidebar-sticky ">
+  <nav class="col-2 col-md-2 bg-light sidebar">
+    <div class="sidebar-sticky">
       <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
         <span>管理員</span>
         <a class="d-flex align-items-center text-muted" href="#">
